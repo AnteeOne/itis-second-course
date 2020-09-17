8b66935b0e5496292addca410266d000107afcfa
Azat Gilyazov <azatpscl@gmail.com>
2020-09-17 20:21:47 +0300

    Add two homeworks
    Add:
    - Second hw (pin link with my bootstrap portfolio)
    - Third hw with color changing js functions!

---
+++ First Semester/HW №3/script.js
@@ -0,0 +1,57 @@
+var content = document.getElementById( 'content' );
+var btns = content.getElementsByTagName( 'button' );
+btns[0].onclick = function() {
+  content.style.backgroundColor = 'red';
+};
+btns[1].onclick = function() {
+  content.style.backgroundColor = 'green';
+};
+btns[2].onclick = function() {
+  content.style.backgroundColor = 'blue';
+};
+btns[3].onclick = function() {
+  // content.style.backgroundColor = 'red';
+  // content.style.backgroundColor = 'orange';
+  // content.style.backgroundColor = 'yellow';
+  // content.style.backgroundColor = 'green';
+  // content.style.backgroundColor = 'blue';
+  // content.style.backgroundColor = 'purple';
+  bg();
+};
+
+var r = 0,
+g = 0,
+b = 0;
+
+function bg () {
+  
+	if (r <= 255 && g == 0 && b == 0) {
+		r ++;
+	}
+
+	if (r == 255 && b == 0 && g <= 255) {
+		g ++;
+	}
+
+	if (r == 255 && g == 255 && b <= 255) {
+		b ++;
+	}
+
+	if (b == 255 && g == 255 && r > 0) {
+		r --;
+	}
+
+	if (r == 0 && b == 255 && g > 0) {
+		g --;
+	}
+
+	if (r == 0 && g == 0 && b > 0) {
+		b --;
+	}
+
+	setTimeout(function() {
+		bg();
+	}, 10);
+
+	content.style.backgroundColor = 'rgb('+r+','+g+','+b+')';
+}	
